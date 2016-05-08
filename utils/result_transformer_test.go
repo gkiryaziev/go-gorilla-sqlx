67883aa5a23c9175a2db8a5d7f436d79fcf4d81d
gkiryaziev <linko@ukr.net>
2016-05-08 19:12:57 +0300

    [ADD] add test

---
+++ utils/result_transformer_test.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestToJson(t *testing.T) {
+	var tests = []struct {
+		data     map[string]interface{}
+		expected string
+	}{{
+		data:     map[string]interface{}{"message": "hello"},
+		expected: `{"message":"hello"}`,
+	}, {
+		data:     map[string]interface{}{"message": 100},
+		expected: `{"message":100}`,
+	}}
+
+	for _, test := range tests {
+		result, err := NewResultTransformer(test.data).ToJson()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if test.expected != result {
+			t.Error(test.expected, "!=", result)
+		}
+	}
+}
